62f5eed0c066e74b84c7196d2aff359967f8d82f
yogeeta <ykutta2@uic.edu>
2016-11-29 14:57:35 -0600

    New User IDs to scrape-Elgin

--- code/Web Scrappers/user_id_prep.py
+++ code/Web Scrappers/user_id_prep.py
@@ -1,7 +1,7 @@
 import csv
 user_ids_list=[]
 
-with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/naperville/Sentiment Analysis/naperville_reviews_data_part2_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile:
+with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/elgin/Sentiment Analysis/elgin_reviews_part10_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile:
   reader = csv.DictReader(readfile)
   for row in reader:
     if row['need_inspection'] == 'YES':
@@ -10,26 +10,26 @@ with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/naperville/Sen
       else:
         continue
 
-##with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/chicago/Sentiment Analysis/chicago_reviews_part5_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile1:
-##  reader1 = csv.DictReader(readfile1)
-##  for row in reader1:
-##    if row['need_inspection'] == 'YES':
-##      if row['user_id'] not in user_ids_list:
-##        user_ids_list.append(row['user_id'])
-##      else:
-##        continue
-##
-##with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/chicago/Sentiment Analysis/chicago_reviews_part6_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile2:
-##  reader2 = csv.DictReader(readfile2)
-##  for row in reader2:
-##    if row['need_inspection'] == 'YES':
-##      if row['user_id'] not in user_ids_list:
-##        user_ids_list.append(row['user_id'])
-##      else:
-##        continue
+with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/elgin/Sentiment Analysis/elgin_reviews_part11_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile1:
+  reader1 = csv.DictReader(readfile1)
+  for row in reader1:
+    if row['need_inspection'] == 'YES':
+      if row['user_id'] not in user_ids_list:
+        user_ids_list.append(row['user_id'])
+      else:
+        continue
+
+with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/ready_data/elgin/Sentiment Analysis/elgin_reviews_part12_dict_sentiment_Data.csv',encoding='ISO-8859-1') as readfile2:
+  reader2 = csv.DictReader(readfile2)
+  for row in reader2:
+    if row['need_inspection'] == 'YES':
+      if row['user_id'] not in user_ids_list:
+        user_ids_list.append(row['user_id'])
+      else:
+        continue
 
 
-with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/user_data_to_scrape/user_ids_naperville_part2.csv', 'w',encoding='ISO-8859-1',newline='') as csvwriterfile:
+with open('/Users/apple/Desktop/YELP_REVIEWS/code/data/user_data_to_scrape/user_ids_elgin_part4.csv', 'w',encoding='ISO-8859-1',newline='') as csvwriterfile:
   writer = csv.writer(csvwriterfile, dialect='excel')
   header_row=["S_No","user_id"]
   writer.writerow(header_row)
